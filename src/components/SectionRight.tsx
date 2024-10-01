b1bf0e6ec8fc2a3b2a9c3796f953f3621c7af4d1
Norluis Galvez <norly021196@gmail.com>
2024-10-01 02:08:32 -0400

    add empty data

--- src/components/SectionRight.tsx
+++ src/components/SectionRight.tsx
@@ -6,7 +6,13 @@ const SectionRight = () => {
   return (
     <div className="lg:px-20 h-full">
       <div className="bg-dark-beige rounded-lg py-20 h-full w-auto">
-        {data === undefined ? <h1>Esta vacio</h1> : <Card />}
+        {data === undefined ? (
+          <h1 className="h-full w-full text-center text-4xl xl:text-9xl font-bold flex items-center justify-center">
+            Empty data
+          </h1>
+        ) : (
+          <Card />
+        )}
       </div>
     </div>
   );
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,5 +1,4 @@
 import { useWeatherContext } from "../../context/WeatherContext";
-import { CurrentWeather, DailyWeather } from "../../interface/index";
 
 const Card = () => {
   const { country, name, data } = useWeatherContext();
@@ -21,21 +20,33 @@ const Card = () => {
       {/* Principal Card */}
       <div className="relative text-light-white bg-green-card rounded-xl shadow-xl p-6 h-full w-full flex flex-col items-center justify-between">
         <div className="flex flex-col items-center">
-          <h2 className="text-xl font-bold">{name}</h2>
-          <h3 className="text-gray-400 font-extrabold">{country}</h3>
+          <h2 className="text-2xl sm:text-5xl font-bold">{name}</h2>
+          <h3 className="text-gray-400 text-xl sm:text-2xl font-extrabold">
+            {country}
+          </h3>
         </div>
 
         <div className="flex items-center h-48 flex-wrap justify-center">
           {data !== undefined && (
-            <h1 className="text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-white/20 to-light-white z-10">
-              {data.current.temperature_2m}&deg;
-            </h1>
+            <>
+              <h1 className="text-7xl sm:text-8xl lg:text-9xl font-extrabold text-transparent bg-clip-text bg-gradient-to-t from-white/20 to-light-white z-10">
+                {data.current.temperature_2m}&deg;
+              </h1>
+              <img
+                src="images/logo.svg"
+                alt=""
+                className="z-20 w-24 sm:w-28 lg:w-36 h-auto"
+              />
+            </>
           )}
-          <img src="images/logo.svg" alt="" className=" z-20 w-36 h-auto" />
         </div>
-        <div className="flex gap-8">
-          <div className="flex flex-col items-center font-bold">
-            <img src="images/logo.svg" alt="" />
+        <div className="flex flex-wrap justify-center gap-8">
+          <div className="flex flex-col text-sm sm:text-lg items-center font-bold">
+            <img
+              src="images/logo.svg"
+              alt=""
+              className="w-16 sm:w-20 lg:w-full"
+            />
             {data !== undefined && (
               <>
                 <p className="text-gray-400">
@@ -46,8 +57,12 @@ const Card = () => {
               </>
             )}
           </div>
-          <div className="flex flex-col items-center font-bold">
-            <img src="images/logo.svg" alt="" />
+          <div className="flex flex-col text-sm sm:text-lg items-center font-bold">
+            <img
+              src="images/logo.svg"
+              alt=""
+              className="w-16 sm:w-20 lg:w-full"
+            />
             {data !== undefined && (
               <>
                 <p className="text-gray-400">
